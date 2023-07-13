64b82f2ba1c1343ece2b7e3bf592b2a3cec58e38
Iuliia Sensi <sensi.julia@gmail.com>
2023-07-13 16:01:07 +0200

    Time: 98 ms (34.06%) | Memory: 44.2 MB (87.28%) - LeetSync

--- ransom-note/ransom-note.js
+++ ransom-note/ransom-note.js
@@ -4,28 +4,23 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    let letters = new Map();
+    let letters = {};
 
     //create hash map
-    for(let i = 0; i < magazine.length; i++) {
-        const key = magazine[i];
-        if(letters.has(key)) {
-            letters.set(key, letters.get(key) + 1)
+    for(let char of magazine) {
+        if(letters[char]) {
+            letters[char]++;
         } else {
-            letters.set(key, 1);
+            letters[char] = 1;
         }
     }
 
     //check all letters 
-    for(let i = 0; i < ransomNote.length; i++) {
-        const letter = ransomNote[i];
-        if(!letters.has(letter)) return false;
-        if (letters.get(letter) === 1) {
-            letters.delete(letter)
-        } else {
-            letters.set(letter, letters.get(letter) - 1)
-        }
+    for(let char of ransomNote) {
+        if(!letters[char]) return false;
+        letters[char]--;
     }
+    console.log(letters)
     return true;
 
 };
