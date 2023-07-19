a72271940c7d920e906ea5f81a83e18693bb5151
Iuliia Sensi <sensi.julia@gmail.com>
2023-07-19 19:56:59 +0200

    Time: 98 ms (55.14%) | Memory: 51.4 MB (73.71%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,14 +4,6 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    
     k = k % nums.length;
     nums.unshift(...nums.splice(nums.length - k, k));
-
-    /*for(let i = 0; i < k; i++) {
-        nums.unshift(nums[nums.length - 1]);
-        nums.pop();
-    }*/
-
-
 };
