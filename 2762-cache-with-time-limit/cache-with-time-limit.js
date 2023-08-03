63e5f8741db6f2bf30178b67ed945cbf46ab85fc
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-03 14:22:18 +0200

    Time: 55 ms (87.70%) | Memory: 42.1 MB (28.20%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,48 @@
+var TimeLimitedCache = function() {
+    this.map = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let exists = this.map.has(key);
+    if(exists) {
+        clearTimeout(this.map.get(key).timer);
+    } 
+    this.map.set(key, {
+        value: value, 
+        timer: setTimeout(() => {
+            clearTimeout(this.map.get(key).timer);
+            this.map.delete(key)}, duration)
+    });
+
+    return exists;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    let val = this.map.get(key);
+    return val ? val.value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.map.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
