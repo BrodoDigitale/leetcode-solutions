e7462850f8736585e1c0e707fddbeb720d8ee3d2
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-19 13:27:03 +0200

    Time: 60 ms (74.84%) | Memory: 43.8 MB (81.57%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    if(!list1 || !list2) return list1 || list2;
+
+    const mergedHead = new ListNode();
+    let node = mergedHead;
+
+    while (list1 && list2) {
+        if(list1.val > list2.val) {
+            node.next = list2;
+            list2 = list2.next;
+        } else {
+            node.next = list1;
+            list1 = list1.next;
+        }
+        node = node.next;
+    }
+
+    node.next = list1 || list2;
+
+    return mergedHead.next;
+
+};
