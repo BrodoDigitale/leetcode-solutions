bebbe4d399f9036f27046a568f85f7537b142672
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-21 11:16:10 +0200

    Time: 56 ms (71.26%) | Memory: 44.7 MB (15.92%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,5 +3,29 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.split(' ').filter(i => i.length > 0).reverse().join(' ');
+    //return s.split(' ').filter(i => i.length > 0).reverse().join(' ');
+
+    let words = [];
+    let word = [];
+
+    //iterate through string
+    for(char of s) {
+        if(char === " ") {
+            //there is space and word contains letters, so this is the end of the word
+            if(word.length > 0) {
+                words.unshift(word.join(""))
+                word = [];
+            }
+        } else {
+            //continue writing to word
+            word.push(char);
+        }
+    }
+
+    //adding last word
+    if(word.length > 0) {
+        words.unshift(word.join(""))
+    }
+
+    return words.join(" ");
 };
