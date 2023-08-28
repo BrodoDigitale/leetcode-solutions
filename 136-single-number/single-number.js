429b9175884fb8c6e065ef413ce30401cd3637a2
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-28 18:29:00 +0200

    Time: 59 ms (76.44%) | Memory: 43.3 MB (75.53%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,17 +3,7 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let idx = 0;
-    while(idx < nums.length) {
-        let curr  = nums[idx];
-        let count = 0;
-        for(let i = 0; i < nums.length; i++) {
-            if (nums[i] === curr) {
-                count += 1;
-            } 
-        }
-        if(count === 1) return curr;
-        idx+=1;
-    }
-    return res;
+
+    //use case for bitwise XOR operator
+    return nums.reduce((a,b) => a ^ b);
 };
