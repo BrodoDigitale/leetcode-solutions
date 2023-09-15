3f18b5f61bc26799aaa8e5a6920b30d0bb697ae1
Iuliia Sensi <sensi.julia@gmail.com>
2023-09-15 11:15:42 +0200

    Time: 160 ms (33.37%) | Memory: 99 MB (45.98%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -5,6 +5,19 @@
  */
 var flat = function (arr, n) {
 
-    return arr.flat(n);
-    
+    if(n === 0) {
+        return arr;
+    }
+
+    let resArr = [];
+
+    for(let i = 0; i <arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            resArr.push(...flat(arr[i], n - 1));
+
+        } else {
+            resArr.push(arr[i]);
+        }
+    }
+    return resArr;
 };
