907dfa8b1da3d4572e65e457c8b9de46cbd4a449
Iuliia Sensi <sensi.julia@gmail.com>
2024-01-06 18:04:50 +0100

    Time: 336 ms (5.36%), Space: 61.5 MB (6.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
@@ -1,3 +1,25 @@
 function sortedSquares(nums: number[]): number[] {
-    return nums.map(n => n*n).sort((a, b) => a - b);
+  
+    const res = [];
+    
+    //2 pointers, left and right
+    let l = 0;
+    let r = nums.length - 1;
+ 
+    
+    //array is sorted but we have negative numbers so we should start 
+    //inserting numbers into res array from the end
+    
+    for(let i = r; i >= 0; i-- ) {
+        console.log(r, l)
+      if(Math.abs(nums[l]) > Math.abs(nums[r])) {
+          res[i] = nums[l] ** 2;
+          l++;
+      } else {
+          res[i] = nums[r] **2;
+          r--;
+      }
+    }
+    
+    return res;
 };
